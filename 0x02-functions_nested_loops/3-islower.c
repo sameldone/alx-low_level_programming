330d0b14d756b20ed987645381f90926e811246f
sameldone <sameldone1@gmail.com>
2023-02-21 01:33:20 -0500

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _islower - Entry point
- * Description: checks is a character is lowercase
+ * Description: checks if a character is lowercase
  * @c: the integer value it receives
  * Return: 1 if true. 0 if false.
  */
 int _islower(int c)
 {
-int i = 'a';
-
-for (i = 'a'; i <= 'z'; i++)
-{
-/* refer int c*/
-if (c == i)
-{
-return (1);
-}
-}
-return (0);
+	int i = 'a';
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		/* refer int c*/
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
