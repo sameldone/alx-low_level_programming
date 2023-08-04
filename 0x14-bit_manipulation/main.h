61f9590ff59f5bbd21e7102e5f820416b2f9647c
sameldone <sameldone1@gmail.com>
2023-08-04 13:58:33 +0300

    My first commit

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifdef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+/* function prototypes*/
+unsigned in binary_to_uint(const char *b);
+#endif
