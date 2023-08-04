a295864fccdcf4b9e09a80362dce1607f3bac830
sameldone <sameldone1@gmail.com>
2023-08-04 14:43:44 +0300

    COrrecting betty1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,18 @@
-#ifdef HEADER_H
+#ifndef HEADER_H
 #define HEADER_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> /* NULL */
 
+/* helper function */
 int _putchar(char c);
 
-/* function prototypes*/
-unsigned in binary_to_uint(const char *b);
+/* function prototypes for all files */
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
 #endif
