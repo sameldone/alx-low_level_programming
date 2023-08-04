feeae644203d49afb8687041d6e79ffe6ac52278
sameldone <sameldone1@gmail.com>
2023-08-04 14:05:19 +0300

    COrrecting betty1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
-unsigned int binary_to_uint(const char *b) {
-  /* Check if the input is NULL. */
-  if (b == NULL) {
+unsigned int binary_to_uint(const char *b) 
+{
+/* Check if the input is NULL. */
+  if (b == NULL)
+  {
     return 0;
   }
-
-  /* Initialize the result. */
+ /* Initialize the result. */
   unsigned int result = 0;
 
   /* Iterate over the input string, adding each digit to the result. */
-  for (int i = 0; b[i] != '\0'; i++) {
+  for (int i = 0; b[i] != '\0'; i++) 
+  {
     /* Check if the digit is valid. */
-    if (b[i] != '0' && b[i] != '1') {
+    if (b[i] != '0' && b[i] != '1') 
+    {
       return 0;
     }
 
