2a48290fb2df41453b3e2e81e6bd327cb8dc39a8
Sameldone <sameldone1@gmail.com>
2024-01-10 17:37:12 +0300

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a newline.
+ * @s: The string to print.
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
+
+    _putchar(*s);
+    _puts_recursion(s + 1);
+}
+
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a single character to stdout.
+ * @c: The character to write.
+ *
+ * Return: 1 on success, -1 on error (errno is set appropriately).
+ */
+int _putchar(char c)
+{
+    /*Directly return the result of write() for clarity and efficiency*/
+    return write(STDOUT_FILENO, &c, 1);
+}
+
